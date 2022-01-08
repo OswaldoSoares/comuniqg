277f0a2740a15577364fd1d4741dbb464418843e
OswaldoSoares <oswaldosoares@gmail.com>
2022-01-07 21:19:30 -0300

    Create apps account, clientes, core, faturamento, produtos, servicos and tabelas

--- comuniqg/settings.py
+++ comuniqg/settings.py
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # my apps
+    'account'
+    'clientes'
+    'core'
+    'faturamento'
+    'produtos'
+    'servicos'
+    'tabelas'
 ]
 
 MIDDLEWARE = [
