7a0068255a637733478c20860fbe2170e82ac775
OswaldoSoares <oswaldosoares@gmail.com>
2022-01-12 20:20:18 -0300

    Settings - STATIC_ROOT

--- comuniqg/settings.py
+++ comuniqg/settings.py
@@ -10,11 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -124,7 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/comuniqg/static'
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
+print(STATIC_ROOT)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
