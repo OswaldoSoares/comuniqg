64e46603ea92daac7fa5425c4e34ad90818d5a97
OswaldoSoares <oswaldosoares@gmail.com>
2022-01-12 21:04:17 -0300

    Modified wsgi.py

--- comuniqg/wsgi.py
+++ comuniqg/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'comuniqg.production')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'comuniqg.development')
 
 application = get_wsgi_application()
