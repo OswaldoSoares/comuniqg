bc1bf3eac3a0c4af358de63ce3667c7d6fc91a35
OswaldoSoares <oswaldosoares@gmail.com>
2022-02-15 10:27:01 -0300

    Selected colors defaults

--- comuniqg/settings.py
+++ comuniqg/settings.py
@@ -125,7 +125,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
-print(STATIC_ROOT)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
