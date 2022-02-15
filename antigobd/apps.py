64a9d7592a8f3df46891832f5edca45238292e57
OswaldoSoares <oswaldosoares@gmail.com>
2022-02-15 11:57:37 -0300

    Create app antigobd, Access to the old DataBase

---
+++ antigobd/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AntigobdConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'antigobd'
--- comuniqg/settings.py
+++ comuniqg/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # my apps
+    'antigobd'
     'account',
     'clientes',
     'core',
