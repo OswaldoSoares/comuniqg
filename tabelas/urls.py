495aa636be7768402a12b6d23f984bef0cbc5f65
OswaldoSoares <oswaldosoares@gmail.com>
2022-02-16 20:11:46 -0300

    Create tabelas.views function delete_tabela, and path in tabelas.urls

--- tabelas/urls.py
+++ tabelas/urls.py
@@ -1,6 +1,8 @@
+from unicodedata import name
 from django.urls import path
-from tabelas.views import index_tabela
+from tabelas.views import index_tabela, delete_tabela
 
 urlpatterns = [
-    path('', index_tabela, name='index_tabela')
+    path('', index_tabela, name='index_tabela'),
+    path('delete_tabela/', delete_tabela, name='delete_tabela'),
 ]
--- tabelas/views.py
+++ tabelas/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-from tabelas.facade import context, get_cliente_tabela
+from tabelas.facade import context, get_cliente_tabela, delete_cliente_tabela
 
 def index_tabela(request):
     contexto = context()
     return render(request, 'tabelas/index.html', contexto)
+
+
+def delete_tabela(request):
+    v_idcadastro = request.GET.get('idcadastro')
+    delete_cliente_tabela(v_idcadastro)
+
