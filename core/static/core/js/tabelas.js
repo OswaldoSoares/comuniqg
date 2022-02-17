200af85594a5ff2639a18b7600c38cae5f04742d
OswaldoSoares <oswaldosoares@gmail.com>
2022-02-16 21:09:23 -0300

    Create js function .tp-delete

--- core/static/core/js/tabelas.js
+++ core/static/core/js/tabelas.js
@@ -1,5 +1,20 @@
 $(document).ready(function(){
     $(document).on('click', '.tp-delete', function() {
-        alert($(this).attr('data-idcadastro'))
+        var idcadastro = $(this).attr('data-idcadastro')
+        $.ajax({
+            type: 'GET',
+            url: 'delete_tabela',
+            data: {
+                idcadastro: idcadastro,
+            },
+            beforeSend: function(){
+                $('.tabela-propria').fadeOut(500)
+            },
+            success:function(data) {
+                $('.tabela-propria').html('')
+                $('.tabela-propria').html(data['html_tabela_propria'])
+                $('.tabela-propria').fadeIn(500)
+            }
+        });
     });
 })
