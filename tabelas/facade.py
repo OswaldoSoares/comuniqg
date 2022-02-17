e045c9b21c5c14f95fcbe345c375364a7f0fea10
OswaldoSoares <oswaldosoares@gmail.com>
2022-02-17 14:22:04 -0300

    Create tabelas.facade function get_produto_tabela, modified function context, create emplate tabela_selecionada, modified style.css

--- tabelas/facade.py
+++ tabelas/facade.py
@@ -1,12 +1,13 @@
 from django.http import JsonResponse
-from databaseold.models import Tabela, Pessoa
+from databaseold.models import Tabela, Pessoa, Produto
 from django.template.loader import render_to_string
 
 
 def context():
     clientes = get_clientes()
     clientes_tabela = get_cliente_tabela()
-    context = {'clientes': clientes, 'clientes_tabela': clientes_tabela}
+    produtos_tabela = get_produto_tabela()
+    context = {'clientes': clientes, 'clientes_tabela': clientes_tabela, 'produtos_tabela': produtos_tabela}
     return context
 
 
@@ -32,6 +33,13 @@ def get_cliente_tabela():
             lista.append({'idcadastro': x['idcadastro'], 'apelido': search_cliente[0].apelido})
     sorted_list = sorted(lista, key=lambda x: x['apelido'])
     return sorted_list
+
+
+def get_produto_tabela():
+    produtos = Produto.objects.all().order_by('categoria', 'descricao')
+    lista = [{'idproduto': itens.idproduto, 'codigo': itens.codigo, 'descricao': itens.descricao,
+             'categoria': itens.categoria, 'valor': itens.valor} for itens in produtos]
+    return lista
 
 
 def delete_cliente_tabela(id_cadastro):
