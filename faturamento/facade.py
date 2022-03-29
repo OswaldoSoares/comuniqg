cfb4c76060e8bc60bdba7bfc347680c2a8de93fa
OswaldoSoares <oswaldosoares@gmail.com>
2022-03-29 10:53:35 -0300

    Faturamentos, get totals faturada, pago and receber

--- faturamento/facade.py
+++ faturamento/facade.py
@@ -1,11 +1,31 @@
-from databaseold.models import Receber
+from django.db.models import Sum
+from databaseold.models import Formapgto, Receber
+
+class FaturasReceber:
+    def __init__(self) -> None:
+        pass
+    
 
 def context():
     faturadas = get_faturadas()
-    return {'faturadas': faturadas}
+    total_faturadas = get_total_faturadas()
+    total_pago = get_total_pago()
+    total_recebe = total_faturadas - total_pago
+    return {'faturadas': faturadas, 'total_faturadas': total_faturadas, 'total_pago': total_pago, 'total_recebe': total_recebe}
 
 
 def get_faturadas():
     faturas = Receber.objects.filter(status='A RECEBER')
     lista = [{'idfatura': itens.idfatura, 'diafatura': itens.diafatura, 'valorfatura': itens.valorfatura, 'valorpago': itens.valorpago} for itens in faturas]
     return lista
+
+
+def get_total_faturadas():
+    total = Receber.objects.filter(status='A RECEBER').aggregate(total=Sum('valorfatura'))
+    return total['total']
+
+
+def get_total_pago():
+    total = Receber.objects.filter(status='A RECEBER').aggregate(pago=Sum('valorpago'))
+    return total['pago']
+
