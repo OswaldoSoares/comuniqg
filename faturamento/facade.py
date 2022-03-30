6bc07503db54033b9567179e9245fd232a277154
OswaldoSoares <oswaldosoares@gmail.com>
2022-03-29 23:37:20 -0300

    Faturadas grouped by clientes

--- faturamento/facade.py
+++ faturamento/facade.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 from django.db.models import Sum
 from databaseold.models import Formapgto, Pessoa, Receber, Servico
 from django.db import connection
@@ -17,16 +18,27 @@ def context():
 
 def get_faturadas():
     faturas = Receber.objects.filter(status='A RECEBER')
-    servico = get_servico_fatura()
     lista = []
+    lista_soma = []
     for itens in faturas:
-        apelido = ' '
-        os = servico.filter(idfatura=itens.idfatura)
+        apelido = ''
+        os = Servico.objects.filter(idfatura=itens.idfatura)
         if os:
             cliente = Pessoa.objects.get(idpessoa=os[0].idcadastro)
             apelido = cliente.apelido
-        lista.append({'idfatura': itens.idfatura, 'diafatura': itens.diafatura, 'valorfatura': itens.valorfatura, 'valorpago': itens.valorpago, 'apelido': apelido})
-    return lista
+        lista.append({'idfatura': itens.idfatura, 'valorfatura': itens.valorfatura, 'valorpago': itens.valorpago, 'apelido': apelido})
+    sorted_list = sorted(lista, key=lambda x: x['apelido'])
+    for itens in sorted_list:
+        lista_cliente = list(filter(lambda x: x['apelido'] == itens['apelido'], sorted_list))
+        soma_fatura = Decimal()
+        soma_pago = Decimal()
+        for x in lista_cliente:
+            soma_fatura += x['valorfatura']
+            soma_pago += x['valorpago']
+        verifica_lista_soma = next((i for i, x in enumerate(lista_soma) if x['apelido'] == itens['apelido']), None)
+        if verifica_lista_soma == None:
+            lista_soma.append({'idfatura': itens['idfatura'], 'valorfatura': soma_fatura, 'valorpago': soma_pago, 'apelido': itens['apelido']})
+    return lista_soma
 
 
 def get_total_faturadas():
