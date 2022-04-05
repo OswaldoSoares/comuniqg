017ca96903a819532aa2aa40e584d1c4381eb4b2
OswaldoSoares <oswaldosoares@gmail.com>
2022-04-04 23:49:45 -0300

    Installed pytest and updated requirements.txt file

--- comuniqg/settings.py
+++ comuniqg/settings.py
@@ -1,98 +1,99 @@
 from ntpath import join
 from pathlib import Path
+
 from decouple import config
 from django.conf.locale.pt_BR import formats as br_formats
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config("SECRET_KEY")
 
-DEBUG = config('DEBUG')
+DEBUG = config("DEBUG")
 
-ALLOWED_HOSTS = [config('ALLOWED_HOSTS')]
+ALLOWED_HOSTS = [config("ALLOWED_HOSTS")]
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django.contrib.humanize',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "django.contrib.humanize",
     # my apps
-    'account',
-    'clientes',
-    'core',
-    'dashboard',
-    'databaseold',
-    'faturamento',
-    'produtos',
-    'servicos',
-    'tabelas',
+    "account",
+    "clientes",
+    "core",
+    "dashboard",
+    "databaseold",
+    "faturamento",
+    "produtos",
+    "servicos",
+    "tabelas",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'comuniqg.urls'
+ROOT_URLCONF = "comuniqg.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'comuniqg.wsgi.application'
+WSGI_APPLICATION = "comuniqg.wsgi.application"
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'USER': config('USER'),
-        'PASSWORD': config('PASSWORD'),
-        'HOST': config('HOST'),
-        'PORT': config('PORT'),
-        'NAME': config('NAME'),
-        'options': {
-            'init_command': 'SET foreing_key_checks = 0',
-        }
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "USER": config("USER"),
+        "PASSWORD": config("PASSWORD"),
+        "HOST": config("HOST"),
+        "PORT": config("PORT"),
+        "NAME": config("NAME"),
+        "options": {
+            "init_command": "SET foreing_key_checks = 0",
+        },
     }
 }
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
-LANGUAGE_CODE = 'pt-br'
+LANGUAGE_CODE = "pt-br"
 
-TIME_ZONE = 'America/Sao_Paulo'
+TIME_ZONE = "America/Sao_Paulo"
 
 USE_I18N = True
 
@@ -100,10 +101,10 @@ USE_L10N = True
 
 USE_TZ = True
 
-br_formats.DATE_FORMAT = 'd/m/Y'
+br_formats.DATE_FORMAT = "d/m/Y"
 
-STATIC_URL = 'static/'
-STATIC_ROOT = config('STATIC_ROOT_CONFIG')
+STATIC_URL = "static/"
+STATIC_ROOT = config("STATIC_ROOT_CONFIG")
 
-MEDIA_URL = 'media/'
-MEDIA_ROOT = config('MEDIA_ROOT_CONFIG')
+MEDIA_URL = "media/"
+MEDIA_ROOT = config("MEDIA_ROOT_CONFIG")
