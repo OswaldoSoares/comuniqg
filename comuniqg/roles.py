dacc1f77bf6700aa2e71b49058c073946bcbc101
OswaldoSoares <oswaldosoares@gmail.com>
2025-06-01 16:53:16 -0300

    Created the UsuarioMaster class

--- comuniqg/roles.py
+++ comuniqg/roles.py
@@ -2,3 +2,7 @@ from rolepermissions.roles import AbstractUserRole
 
 module_servicos = "module_servicos"
 
+class UsuarioMaster(AbstractUserRole):
+    available_permissions = {
+        module_servicos: True,
+    }
